224252e7bcf6f18ee13df6394caaf8035d3f901d
Harry Sudana <harrysudana@gmail.com>
2024-06-12 14:13:38 +0800

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -1,14 +1,7 @@
 echo "** Creating default DB and users"
 
-mysql -u root -p${DB_ROOT_PASSWORD} <<-EOF
+mariadb -uroot -p${DB_ROOT_PASSWORD} -e "
 GRANT ALL PRIVILEGES ON  \`${DB_USER_NAME}\\_%\`.* TO  '${DB_USER_NAME}'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-#mysql -u root -p${DB_ROOT_PASSWORD} --execute \
-#"CREATE DATABASE IF NOT EXISTS ${DB_USER_NAME}_test; 
-#GRANT ALL PRIVILEGES ON \`${DB_USER_NAME}\\_%\`.* TO '${DB_USER_NAME}'@'%';
-#DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-#FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;"
 
 echo "** Finished creating default DB and users"
